e90439d8264a5900110a14fda78307ee16a3694a
David Adokuru <davidadokuru@gmail.com>
2023-01-24 11:50:33 +0100

    updates

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -13,11 +13,21 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Mail;
 use Twilio\Rest\Client;
+use Kreait\Firebase\Contract\Messaging;
+use Kreait\Firebase\Messaging\CloudMessage;
+
+
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
+    public Messaging $messaging;
+
+    public function __construct(Messaging $messaging)
+    {
+        $this->messaging = $messaging;
+    }
 
     public function sendSMSMessage($to, $message)
     {
@@ -137,116 +147,13 @@ class Controller extends BaseController
 
         DB::table('notifications')->insert(['user_id' => $userID, 'title' => $data['title'], 'message' => $data['message'], 'type' => 3]);
 
+        $user = User::find($userID);
 
-        // /************************Notification Start************/
+        $token = $user->device_token;
 
-        // $firebaseToken =  DB::table('users')->where('id', Auth::id())->whereNotNull('device_token')->pluck('device_token')->toArray();
+        $message = CloudMessage::withTarget('token', $token)
+            ->withNotification($data);
 
-        // $SERVER_API_KEY = 'AAAAz7uAT0o:APA91bEERv13lfoUEgsFx_Bjc8TaqxWe7hYj6QrmT4Di5AoLchYkO-oOHpmBTAo6ZMB1293LJe_LIXeZKilg_UikEeSQWyZchAyqcmlKfGrBa0IBoKjAKX9GnrCHesIo0oEnNME5nIJ8';
-
-        // $data1 = [
-        //     "registration_ids" => $firebaseToken,
-        //     "notification" => [
-        //         "title" => $data_dtiver['title'],
-        //         "body" => $data_dtiver['message'],
-        //     ]
-        // ];
-        // $dataString = json_encode($data1);
-
-        // $headers = [
-        //     'Authorization: key=' . $SERVER_API_KEY,
-        //     'Content-Type: application/json',
-        // ];
-
-        // $url = 'https://fcm.googleapis.com/fcm/send';
-        // $ch = curl_init();
-
-        // curl_setopt($ch, CURLOPT_URL, $url);
-        // curl_setopt($ch, CURLOPT_POST, true);
-        // curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        // curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        // curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-        // curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
-        // // Disabling SSL Certificate support temporarly
-        // curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        // curl_setopt($ch, CURLOPT_POSTFIELDS, $dataString);
-        // // Execute post
-        // $result = curl_exec($ch);
-        // curl_close($ch);
-
-        // /*********************End Notification*****************/
-        // DB::table('notifications')->insert(['user_id' => $data_dtiver['user_id'], 'title' => $data_dtiver['title'], 'message' => $data_dtiver['message'], 'type' => 1]);
-
-
-        // $data_serviceprovider = array('title' => "New Booking", 'message' => " $bookingId New Booking Comes From the User", 'user_id' => $serviceproviderId);
-
-
-        // /************************Notification Start************/
-
-        // $firebaseTokenone =  DB::table('users')->where('id', $serviceproviderId)->whereNotNull('device_token')->pluck('device_token')->toArray();
-
-        // $SERVER_API_KEY = 'AAAAz7uAT0o:APA91bEERv13lfoUEgsFx_Bjc8TaqxWe7hYj6QrmT4Di5AoLchYkO-oOHpmBTAo6ZMB1293LJe_LIXeZKilg_UikEeSQWyZchAyqcmlKfGrBa0IBoKjAKX9GnrCHesIo0oEnNME5nIJ8';
-
-        // $data2 = [
-        //     "registration_ids" => $firebaseTokenone,
-        //     "notification" => [
-        //         "title" => $data_serviceprovider['title'],
-        //         "body" => $data_serviceprovider['message'],
-        //     ]
-        // ];
-        // $dataString2 = json_encode($data2);
-
-        // $headers = [
-        //     'Authorization: key=' . $SERVER_API_KEY,
-        //     'Content-Type: application/json',
-        // ];
-
-        // $url = 'https://fcm.googleapis.com/fcm/send';
-        // $ch = curl_init();
-
-        // curl_setopt($ch, CURLOPT_URL, $url);
-        // curl_setopt($ch, CURLOPT_POST, true);
-        // curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        // curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        // curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-        // curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
-        // // Disabling SSL Certificate support temporarly
-        // curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        // curl_setopt($ch, CURLOPT_POSTFIELDS, $dataString2);
-        // // Execute post
-        // $result = curl_exec($ch);
-        // curl_close($ch);
-
-        // /*********************End Notification*****************/
-
-
-
-        // DB::table('notifications')->insert(['user_id' => $data_serviceprovider['user_id'], 'title' => $data_serviceprovider['title'], 'message' => $data_serviceprovider['message'], 'type' => 3]);
-
-        // /******************  Send notification on mail ***********************************/
-        // //user
-
-        // $get_user_data =  DB::table('users')->select('id', 'email', 'name')->where('id', Auth::id())->first();
-        // if ($get_user_data) {
-        //     $data['name'] = $get_user_data->name;
-        //     $data['msg'] = "Service Booked : your new service booked successfully, Booking Id is " . $bookingId;
-        //     $data['subject'] = "Service Booked";
-
-        //     Mail::to($get_user_data->email)->send(new \App\Mail\SendOrderMail($data));
-        // }
-
-
-        // //service provider
-
-        // $getServiceProvider =  DB::table('users')->select('id', 'email', 'name')->where('id', $serviceproviderId)->first();
-        // if ($getServiceProvider) {
-        //     $data['name'] = $getServiceProvider->name;
-        //     $data['msg'] = "New Service Recieved : your have received new  service successfully, Booking Id is " . $bookingId;
-        //     $data['subject'] = "Service Recieved";
-        //     Mail::to($getServiceProvider->email)->send(new \App\Mail\SendOrderMail($data));
-        // }
-
-
-        /***************   End *********************************************************************/
+        $this->messaging->send($message);
     }
 }
