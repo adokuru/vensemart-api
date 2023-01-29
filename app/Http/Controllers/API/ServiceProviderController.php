3029b96299b79ab4a2ecf0f9d62386ebb5626803
David Adokuru <davidadokuru@gmail.com>
2023-01-29 23:07:35 +0100

    updates

--- app/Http/Controllers/API/ServiceProviderController.php
+++ app/Http/Controllers/API/ServiceProviderController.php
@@ -748,6 +748,7 @@ class ServiceProviderController extends Controller
                 $result[$key]['location'] = $value->location;
                 $result[$key]['service_type_price'] = $value->service_type_price;
                 $result[$key]['booking_count'] = $value->booking_count;
+                $result[$key]['service_provider_rating'] = $value->service_provider_rating;
             }
 
 
