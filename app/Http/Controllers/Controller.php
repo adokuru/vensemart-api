ef0980b210529f951196c5d7b0f5b900505b987a
David Adokuru <davidadokuru@gmail.com>
2023-03-30 16:15:42 +0100

    updates

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -269,7 +269,8 @@ class Controller extends BaseController
             // send notification to rider 
             $this->sendNotification($rider->id, $data['title'], $data['body']);
 
-            $this->sendSMSMessage("234" . substr($rider->phone, -10), $data['body']);
+            $this->sendSMSMessage("234" . substr($rider->mobile, -10), $data['body']);
+
             return $this->sendResponse('Rider requested successfully', []);
         } catch (\Exception $e) {
             throw new \Exception($e->getMessage());
