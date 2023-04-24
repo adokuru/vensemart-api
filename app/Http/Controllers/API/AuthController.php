1f1c55badde20b04dbd0e8d1cb45511f870ade85
Vensemart <ejikeme.evuolu@gmail.com>
2023-04-24 15:48:06 +0100

    test otp

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -83,11 +83,55 @@ class AuthController extends Controller
     public function sendMeMessage(Request $request){
 
 
+        try {
+            $typevalidate = Validator::make($request->all(), [
+                'phone_number' => 'required'
+            ]);
 
-           $phone_Number = '+234' . substr($request->phone_number, -10);
-            $message = "Your Vensemart authentication code is ". ". Please do not share this code with anyone. This code expires in 5 mins.";
+            if ($typevalidate->fails()) {
+                $arr['status'] = 0;
+                $arr['message'] = $typevalidate->errors()->first();
+                $arr['data'] = NULL;
+                return response()->json($arr, 422);
+            }
+
+            // $users = User::where(function ($query) use ($request) {
+            //     $query->orwhere('mobile', $request->phone_number);
+            // })->first();
+
+
+            $users = '07030628145';
+
+            if (!$users) {
+                $arr['status'] = 0;
+                $arr['message'] = 'Phone number not found';
+                $arr['data'] = NULL;
+                return response()->json($arr, 422);
+            }
+
+            // set otp
+
+            $otp = rand(1000, 9999);
+            // $users->otp = $otp;
+            // $users->save();
+
+
+
+            $phone_Number = '+234' . substr($request->phone_number, -10);
+            $message = "Your Vensemart authentication code is " . $otp . ". Please do not share this code with anyone. This code expires in 5 mins.";
 
             $this->sendSMSMessage($phone_Number, $message);
+
+            $arr['status'] = 1;
+            $arr['message'] = 'OTP sent successfully';
+            $arr['data'] = NULL;
+            return response()->json($arr, 200);
+        } catch (\Exception $e) {
+            $arr['status'] = 0;
+            $arr['message'] = $e->getMessage();
+            $arr['data'] = NULL;
+            return response()->json($arr, 500);
+        }
 
     }
 
