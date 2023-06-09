590e24977f11ed6ef33e82248a748cf45cada6b5
mickydamz <mickydamz@gmail.com>
2023-06-09 13:15:23 +0100

    Revert "changes to api"
    This reverts commit 0c53ec469b44db0cbf2c924755bf37c568c807d1.

--- resources/views/manage/serviceorder/cancelled_listing.blade.php
+++ resources/views/manage/serviceorder/cancelled_listing.blade.php
@@ -205,8 +205,6 @@
                        elseif($val->service_type ==53)
                        {?> <span class="">     Shoe Cobbler</span> <?php } 
 
-                       elseif($val->service_type ==54)
-                       {?> <span class="">Interior Decoration</span> <?php } 
                        
                      
           
