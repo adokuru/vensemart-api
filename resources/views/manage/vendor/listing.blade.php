d6aa5e879b07683a92735b4952c2297773588a78
mickydamz <mickydamz@gmail.com>
2023-06-14 13:52:10 +0100

    changes to apicontroller

--- resources/views/manage/vendor/listing.blade.php
+++ resources/views/manage/vendor/listing.blade.php
@@ -51,9 +51,10 @@
                    
                             $burl = str_replace($burl,'Vensemart','vensemart_vendor');
                             $burl1 ='https://vensemart.com/'.$burl;
+                            $burl2 ='https://api.vensemart.com/';
                          //   echo $burl1;
                    ?>
-                   <img src="<?php echo $burl1.'/cmsadmin/warehouse_images/'.$val->image; ?>"  width="30" height="30">
+                   <img src="<?php echo $burl2.'/vendor_images/'.$val->image; ?>"  width="30" height="30">
                     <!--<img src="{{  url('vensemart_vendor/cmsadmin/warehouse_images').'/'. $val->image }}"  width="30" height="30">-->
                    
                     <?php } ?>
