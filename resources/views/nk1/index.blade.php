cb1bcabae577ab71338d3faa15f774f2b0074f0e
mickydamz <mickydamz@gmail.com>
2023-06-19 13:26:54 +0100

    additions to api

--- resources/views/nk1/index.blade.php
+++ resources/views/nk1/index.blade.php
@@ -9,7 +9,7 @@
                                 <div class="nk-block-head nk-block-head-sm">
                                     <div class="nk-block-between">
                                         <div class="nk-block-head-content">
-                                            <h3 class="nk-block-title page-title"> Vensemart Vendor</h3>
+                                            <h3 class="nk-block-title page-title"> Vensemart Vendor Dashboard</h3>
                                             <div class="nk-block-des text-soft">
                                                 <p>Welcome, {{ auth()->user()->first_name }}</p>
                                             </div>
