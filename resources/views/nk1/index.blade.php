1b2a15d9f386e97707982a08132a7fe5d3154ec2
mickydamz <mickydamz@gmail.com>
2023-06-19 13:24:56 +0100

    additions to api

--- resources/views/nk1/index.blade.php
+++ resources/views/nk1/index.blade.php
@@ -11,7 +11,7 @@
                                         <div class="nk-block-head-content">
                                             <h3 class="nk-block-title page-title"> Vensemart Vendor</h3>
                                             <div class="nk-block-des text-soft">
-                                                <p>Welcome to Vendor Dashboard</p>
+                                                <p>Welcome to {{ $first_name }}</p>
                                             </div>
                                         </div><!-- .nk-block-head-content -->
                                         <div class="nk-block-head-content">
