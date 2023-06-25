afefe25a53c2973b071a54b3b7ca73f1a2d5e115
mickydamz <mickydamz@gmail.com>
2023-06-25 16:30:50 +0100

    changes to service provider

--- app/Http/Controllers/API/ServiceProviderController.php
+++ app/Http/Controllers/API/ServiceProviderController.php
@@ -652,6 +652,7 @@ class ServiceProviderController extends Controller
                     )
                     ->where('users.type', 3)
                     ->where('users.service_type', '!=', null)
+                    ->distinct()
                     ->leftJoin('serviceprovider_category', 'serviceprovider_category.id', '=', 'users.service_type')
                     ->leftJoin('servicebook_user', 'servicebook_user.service_pro_id', '=', 'users.id')
                     ->orderBy('distance', 'asc')
