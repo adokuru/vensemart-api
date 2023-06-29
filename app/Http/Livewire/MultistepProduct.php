99ecde7124544029813556ad4ab71ed60ce5455e
mickydamz <mickydamz@gmail.com>
2023-06-29 12:45:06 +0100

    changes to picture quality in multisteppoc

--- app/Http/Livewire/MultistepProduct.php
+++ app/Http/Livewire/MultistepProduct.php
@@ -156,7 +156,7 @@ class MultistepProduct extends Component
 
         $extension = $this->fileName->getClientOriginalExtension(); 
         $resizedImage = Image::make($this->fileName->getRealPath())
-               ->resize(700, 700)
+               ->resize(800, 600)
                ->encode();
 
         $fileNamee = rand(1000,200000000).$this->product_title . '.' . $extension;
