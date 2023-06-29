99166e1ce28a70406f69e6581aef16399b475255
mickydamz <mickydamz@gmail.com>
2023-06-29 16:48:27 +0100

    changes to picture quality in multisteppoc

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <meta name="author" content="Softnio">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="A powerful and conceptual apps base dashboard template that especially build for developers and programmers.">
+    <meta name="description" content="Vensemart Vendor">
     <!-- Fav Icon  -->
     <link rel="shortcut icon" href="./images/favicon.png">
     <!-- Page Title  -->
--- resources/views/nk1/layouts/nk5layout.blade.php
+++ resources/views/nk1/layouts/nk5layout.blade.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <meta name="author" content="Softnio">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="A powerful and conceptual apps base dashboard template that especially build for developers and programmers.">
+    <meta name="description" content="Vensemart Vendor">
     <!-- Fav Icon  -->
     <link rel="shortcut icon" href="{{ asset('https://vensemart.com/blocklive.com/images/favicon.ico') }}">
     <!-- Page Title  -->
