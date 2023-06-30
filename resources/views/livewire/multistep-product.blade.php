02eb81429e3d92f359e9541582604338d239f34c
mickydamz <mickydamz@gmail.com>
2023-06-30 12:07:06 +0100

    changes to picture quality in multisteppoc

--- resources/views/livewire/multistep-product.blade.php
+++ resources/views/livewire/multistep-product.blade.php
@@ -1,11 +1,118 @@
 <div>
     
+
      <form wire:submit.prevent="register">
 
          {{-- STEP 1 --}}
 
          @if ($currentStep == 1)
              
+         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.js"></script>
+
+         <style>
+
+#mask {
+  position: absolute;
+  left: 0;
+  top: 0;
+  z-index: 9000;
+  background-color: #000;
+  display: none;
+}
+
+#boxes .window {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 440px;
+  height: 200px;
+  display: none;
+  z-index: 9999;
+  padding: 20px;
+  border-radius: 15px;
+  text-align: center;
+}
+
+#boxes #dialog {
+  width: 750px;
+  height: 300px;
+  padding: 10px;
+  background-color: #ffffff;
+  font-family: 'Segoe UI Light', sans-serif;
+  font-size: 15pt;
+}
+
+#popupfoot {
+  font-size: 16pt;
+  position: absolute;
+  bottom: 0px;
+  width: 250px;
+  left: 250px;
+}
+
+            </style>
+
+
+<script>
+
+
+$(document).ready(function() {
+  var id = '#dialog';
+
+  // Get the screen height and width
+  var maskHeight = $(document).height();
+  var maskWidth = $(window).width();
+
+  // Set the height and width of the mask to fill up the whole screen
+  $('#mask').css({ 'width': maskWidth, 'height': maskHeight });
+
+  // Transition effect
+  $('#mask').fadeIn(500);
+  $('#mask').fadeTo("slow", 0.9);
+
+  // Get the window height and width
+  var winH = $(window).height();
+  var winW = $(window).width();
+
+  // Set the popup window to center
+  $(id).css('top', winH / 2 - $(id).height() / 2);
+  $(id).css('left', winW / 2 - $(id).width() / 2);
+
+  // Transition effect
+  $(id).fadeIn(2000);
+
+  // If the close button is clicked
+  $('.window .close').click(function(e) {
+    // Cancel the link behavior
+    e.preventDefault();
+
+    $('#mask').hide();
+    $('.window').hide();
+  });
+
+  // If the mask is clicked
+  $('#mask').click(function() {
+    $(this).hide();
+    $('.window').hide();
+  });
+});
+
+</script>
+
+         <div id="boxes">
+  <div id="dialog" class="window">
+    Vensemart takes pride in making sure that our <br/>vendors get that highest quality services and<br/> to help us do this we will take a 
+    2% commission on<br/>  all products sold
+
+    <div id="popupfoot">
+      <a href="#" class="close agree btn btn-success">I agree</a> 
+      <a class="agree btn btn-danger" style="color:white;" href="#">I do not agree</a>
+    </div>
+  </div>
+  <div id="mask"></div>
+</div>
+
+
       
          <div class="step-one">
              <div class="card">
