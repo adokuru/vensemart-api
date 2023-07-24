baec378ed1ca2ab081784b68b7ecb1dd9abea001
mickydamz <mickydamz@gmail.com>
2023-07-24 12:20:28 +0100

    changes to multistepproduct

--- app/Http/Livewire/MultistepProduct.php
+++ app/Http/Livewire/MultistepProduct.php
@@ -168,7 +168,7 @@ class MultistepProduct extends Component
 
         $extension = $this->fileName->getClientOriginalExtension(); 
         $resizedImage = Image::make($this->fileName->getRealPath())
-               ->resize(900, 500)
+               ->resize(500, 664)
                ->encode();
 
         $fileNamee = rand(1000,200000000).$this->product_title . '.' . $extension;
