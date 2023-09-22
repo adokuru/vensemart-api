745e21c96a5e2f3c0b9c32cac27ee91e879ad629
mickydamz <mickydamz@gmail.com>
2023-09-22 07:50:52 +0100

    c hanges to bank accounts

--- resources/views/manage/store/listing.blade.php
+++ resources/views/manage/store/listing.blade.php
@@ -43,11 +43,17 @@
               <tr>
                 <td>{{  $i }}</td>
                 <td>{{ $val->user_id ?? 'N/A' }}</td>
-                <td>{{ $val->username ?? 'N/A' }}</td>
-                 <td>{{ $val->bank_nm ?? 'N/A' }}</td>
-                  <td>{{ $val->acc_name ?? 'N/A' }}</td>
-                  <td>{{ $val->ac_no ?? 'N/A' }}</td>
-                <!-- <td>{{ $val->telephone }}</td> -->
+                @if (empty($val))
+    <td>N/A</td>
+    <td>N/A</td>
+    <td>N/A</td>
+    <td>N/A</td>
+@else
+    <td>{{ $val->user_id }}</td>
+    <td>{{ $val->bank_nm }}</td>
+    <td>{{ $val->acc_name }}</td>
+    <td>{{ $val->ac_no }}</td>
+@endif>
                 
                 <td><?php if($val->is_verified == 1){ ?>
                       <span class="badge badge-success">Verified</span>
