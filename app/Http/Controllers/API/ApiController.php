4d9528b5ad95664f49cbfa58359c2e6b3ff18676
Weki Work labs <ukonuzoidx@gmail.com>
2024-04-21 00:57:02 +0100

    okay

--- app/Http/Controllers/API/ApiController.php
+++ app/Http/Controllers/API/ApiController.php
@@ -1241,7 +1241,7 @@ class ApiController extends Controller
 
                 // if driver is null or not assigned to the order
                 // if ($driver == null) {
-                $test =                $this->contactRiderForDelivery($orders->order_id, $user_id, $ride_request->start_address, $ride_request->end_address, $ride_request->start_latitude, $ride_request->start_longitude);
+                 $this->contactRiderForDelivery($orders->order_id, $user_id, $ride_request->start_address, $ride_request->end_address, $ride_request->start_latitude, $ride_request->start_longitude);
                 // }
 
                 $data = [
