b66ee320a19aa9fe07645d697ceac1efc613317c
Weki Work labs <ukonuzoidx@gmail.com>
2024-04-21 21:32:12 +0100

    omo okay

--- app/Http/Controllers/API/DeliveryRiderController.php
+++ app/Http/Controllers/API/DeliveryRiderController.php
@@ -609,7 +609,7 @@ class DeliveryRiderController extends Controller
             $order = Orders::where('id', $orderid)->where('status', '2')->orWhere('status', '1')->first();
             // $order = DB::table('orders')->where('id', $orderid)->where('status', '2')->where('driver_id', $driverId)->first();
 
-            dd($order->driver_id, $order->status, $order->id, $order->user_id, $order->ride_request_id, $order);
+            dd($orderid, $order->driver_id, $order->status, $order->id, $order->user_id, $order->ride_request_id, $order);
 
 
             // if ($order) {
